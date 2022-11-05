fd6864edb9befd4b5b7d25173926e6442ab78e92
Timothy Graupmann <tgraupmann@gmail.com>
2022-11-04 21:26:47 -0700

    Commented out logging

--- Server/app.js
+++ Server/app.js
@@ -1,23 +1,14 @@
-const fs = require('fs'); //debugging
 const WebSocket = require('ws');
 
 const wss = new WebSocket.WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
-    console.log(new Date(), 'onMessage', data)
+    //console.log(new Date(), 'onMessage', data)
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        console.log(new Date(), 'Send data to clients', data.length, 'bytes');
+        //console.log(new Date(), 'Send data to clients', data.length, 'bytes');
         client.send(data);
-
-        /*
-        fs.writeFile('test.png', data, null, function (err) {
-          if (err) {
-            return console.log(new Date(), 'Failed to write!', err);
-          }
-        });
-        */
       }
     });
   });
